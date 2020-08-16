758701f02dfff9e06875c56b0d64e6a0d6a112fa
Ezeugo Anukem <jea2161@columbia.edu>
2020-08-16 13:17:33 -0700

    added rotting oranges description to ocaml folder

---
+++ interview_questions/ocaml/rotting_oranges.ml
@@ -0,0 +1,9 @@
+(* In a given grid, each cell can have one of three values: *)
+(*  *)
+(* the value 0 representing an empty cell; *)
+(* the value 1 representing a fresh orange; *)
+(* the value 2 representing a rotten orange. *)
+(* Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten. *)
+(* Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead. *)
+
+
