8c56a0609ad3fcdddde7a99f4fa86f76c550e17b
Ezeugo Anukem <jea2161@columbia.edu>
2020-08-22 16:11:45 -0700

    Added pour water solution in ocaml

---
+++ interview_questions/ocaml/pour_water.ml
@@ -0,0 +1,40 @@
+(* See pour_water.py for description *)
+
+(* takes a list of numbers and adds a droplet if possible *)
+(*     to the left of the k *)
+let go_left heights k =
+  let rec go i k =
+    match (-1) != i && heights.(i) <= heights.(k) with
+    | true ->
+      if heights.(i) < heights.(i + 1)
+      then (heights.(i) <- (heights.(i) + 1); (i, true, heights))
+      else go (i - 1) k
+    | false -> (i, false, heights) in
+  go (k - 1) k
+
+
+let print_array arr =
+  print_string "\n";
+  for i = 0 to Array.length  arr - 1 do
+    print_int arr.(i);
+  done
+
+let go_right heights k =
+  let rec go i k =
+    match i != Array.length heights && heights.(i) <= heights.(k) with
+    | true ->
+      if heights.(i) < heights.(i - 1) then
+        (heights.(i) <- (heights.(i) + 1); (i, true, heights))
+      else (go ( i + 1 ) k)
+    | false -> (i, false, heights) in
+  go (k + 1) k
+
+
+let rec pour_water heights k v =
+  match v = 0 with
+  | true -> heights
+  | false -> match go_left heights k with
+    | (i, true, h) -> pour_water h k (v - 1)
+    | (i, false, h) -> match go_right h k with
+      | (i, true, h) -> pour_water h k (v - 1)
+      | (i, false, h) -> h.(k) <- h.(k) + 1; pour_water h k (v - 1)
