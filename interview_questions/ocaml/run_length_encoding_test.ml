65ad32c4b0453d637ffb9fe0e6aa001f1dba1c2e
Ezeugo Anukem <jea2161@columbia.edu>
2021-07-04 22:46:02 -0400

    added another test case

--- interview_questions/ocaml/run_length_encoding_test.ml
+++ interview_questions/ocaml/run_length_encoding_test.ml
@@ -4,6 +4,7 @@ open Run_length_encoding
 let tests = "run length tests" >::: [
     "1"     >:: (fun _ -> assert_equal  [(4, 1);(2,2)] ( encoding [1;1;1;1;2;2] ) );
     "2"     >:: (fun _ -> assert_equal  [(4, 1)] ( encoding [1;1;1;1] ) );
+    "3"     >:: (fun _ -> assert_equal  [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")] ( encoding ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] ) );
 ]
 
 let _ = run_test_tt_main tests
